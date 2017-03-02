fabbbd9dd1702f3f1921ac46484d08cb75215c88
MHaneferd <mhaneferd@gmail.com>
2017-03-02 20:45:23 +0100

    Final Shiny App for Week 4 Peer Review in Coursera course: Data Products

--- BirthRateNorway/server.R
+++ BirthRateNorway/server.R
@@ -1,26 +1,78 @@
 #
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
+# This is the server logic of the Shiny web application 
 #
 
+# Load libraries
 library(shiny)
+library(DMwR)
+library(randomForest)
 
-# Define server logic required to draw a histogram
+# Read the sourcefile 
+filedata <- read.table("data.csv", header = TRUE, sep = ";", dec = ",", quote = "\"")
+
+# Do some cleanup
+rows <- (c("hospital.beds.per.1000","birth.rate.per.1000","population.growth.percent", "population.total","population.female"))
+filedata <- filedata[,-c(1:4)]
+df <- cbind(rows, filedata)
+rows <- c("rows",1960:2016)
+colnames(df) <- rows
+df <- df[, -58]
+
+# transpose the data:
+df <- as.data.frame(t(df))
+rows <- (c("hospital.beds.per.1000","birth.rate.per.1000","population.growth.percent", "population.total","population.female"))
+colnames(df) <- rows
+df <- df[-1, ]
+
+#transform to numeric function
+asNumeric <- function(x) as.numeric(as.character(x))
+factorsNumeric <- function(d) modifyList(d, lapply(d[, sapply(d, is.factor)],asNumeric))
+
+# Change the factors to numeric
+df <- factorsNumeric(df)
+df <- df[,-3 ] # Remove population growth in percent
+
+#impute NA's
+df_imputed <- knnImputation(df,k=2)
+
+#Do a glm
+fit <- glm(birth.rate.per.1000~., data=df_imputed)
+
+# Define server logic required to display the predicted output:
 shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
+         
+      
+output$distPlot <- renderPlot({
     
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
+        
+        # Create newdata based on the range of ten years starting at offset, ending
+        # where the sliders stop :-)
+        df2 = df_imputed[47:56,]
+        df2$hospital.beds.per.1000 <- seq(3.3,input$beds, length.out = 10)
+        df2$population.total <- seq(5195921,input$population,length.out = 10)
+        df2$population.female <- seq(49.62923,input$female,length.out = 10)
+        rownames(df2) <- c(2016:2025) #New years :-)
+
+        # Predict birthrate based on random forest model:
+        df2$birth.rate.per.1000 <- predict.glm(fit,df2)
+        
+        # Do some additional cleanup and formatting
+        df3 <- rbind(df_imputed,df2)
+        df3 <- cbind(df3,as.numeric(rownames(df3)))
+        colnames(df3)[5] <- "year"
+        df2 <- cbind(df2,as.numeric(rownames(df2)))
+        colnames(df2)[5] <- "year"
+        
+        
+        # Plot the birthrate and the predicted ten last years in Red.
+        plot(df3$year, df3$birth.rate.per.1000, 
+             type="l", 
+             col = "blue",
+             xlab = "Year",
+             ylab = "Birth Rate per 1000 people in Norway"
+             )
+        lines(x = df2$year,y = df2$birth.rate.per.1000, col="red", lwd=3)
+        
   })
   
 })
--- BirthRateNorway/ui.R
+++ BirthRateNorway/ui.R
@@ -1,31 +1,36 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI for application that draws a plot
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
+  titlePanel("Estimate Birth Rate in Norway for the next ten years"),
+  h4("You can simulate range of hospital beds, total population and the female percentage of the population in Norway, to see the effect of the birth rate (per 1000 persons)."),
+  h4("The three predictors has been collected from The World Bank of data (http://databank.worldbank.org), and has been fitted in a glm model."),
+  h4("The sliders indicates the year 2026 values. That creates a range from 2016 to 2026 which is used to fit the glm model. The predicted birthrate for next ten years is marked in red in the plot."),
+                
+  # Sidebar with a slider input 
   sidebarLayout(
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+          # Create sliders for the three predictors:
+          sliderInput("beds",
+                      "Hospital beds (per 1000 people):",
+                      min = 1,
+                      max = 15,
+                      value = 3.3),
+          sliderInput("population",
+                      "Population in Norway:",
+                      min = 2000000,
+                      max = 10000000,
+                      value = 5195921),
+          sliderInput("female",
+                      "Population female (% of total):",
+                      min = 40,
+                      max = 60,
+                      value = 49.62923)
     ),
     
-    # Show a plot of the generated distribution
+    # Show a plot
     mainPanel(
        plotOutput("distPlot")
     )
